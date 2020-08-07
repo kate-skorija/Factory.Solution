3f6cc8e00d7552a2eae556a861afde8903732a0a
Kate Skorija <kateskorija@gmail.com>
2020-08-07 08:53:43 -0700

    add properties to Engineer class

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -4,6 +4,13 @@ namespace Factory.Models
 {
   public class Engineer
   {
-    
+    public Engineer()
+    {
+      this.Machines = new HashSet<EngineerMachine>();
+    }
+    public string Name { get; set; }
+    public string Shift { get; set; }
+    public int ExperienceLevel { get; set; }
+    public virtual ICollection<EngineerMachine> Machines { get; set; }
   }
 }
