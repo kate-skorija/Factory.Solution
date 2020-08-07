26c728e25c745e359bf2b9da265971057e79a1ad
Kate Skorija <kateskorija@gmail.com>
2020-08-07 11:52:04 -0700

    create index route and view for machines controller

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller 
+  {
+    private readonly FactoryContext _db;
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      return View(_db.Machines.ToList());
+    }
+  }
+}
