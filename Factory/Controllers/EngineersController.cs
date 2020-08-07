720e072e60b30f8c153eb6a0247c8f82a9f8cf04
Kate Skorija <kateskorija@gmail.com>
2020-08-07 10:53:46 -0700

    create edit routes and view for engineers controller

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -37,5 +37,18 @@ namespace Factory.Controllers
         .FirstOrDefault(engineer => engineer.EngineerId == id);
         return View(thisEngineer);
     }
+    public ActionResult Edit(int id)
+    {
+      var thisCategory = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+      return View(thisCategory);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Engineer engineer)
+    {
+      _db.Entry(engineer).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
